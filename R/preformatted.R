67e9ea7470b1cff4625eb6acb980478e8826437f
Jonathon Love <jon@thon.cc>
2016-11-08 13:38:57 +1100

    Added preformatted results class

---
+++ R/preformatted.R
@@ -0,0 +1,54 @@
+
+Preformatted <- R6::R6Class("Preformatted",
+    inherit=ResultsElement,
+    private=list(
+        .content=''),
+    active=list(
+        content=function(value) {
+            if (base::missing(value))
+                return(private$.content)
+            private$.content <- value
+            base::invisible(self)
+        }
+    ),
+    public=list(
+        initialize=function(
+            options,
+            name='',
+            title='',
+            visible=TRUE,
+            clearWith=NULL) {
+
+            super$initialize(
+                options=options,
+                name=name,
+                title=title,
+                visible=visible,
+                clearWith=clearWith)
+        },
+        asString=function() {
+            private$.content
+        },
+        fromProtoBuf=function(element, oChanges=NULL, vChanges=NULL) {
+            if ( ! base::inherits(element, "Message"))
+                reject("Table$fromProtoBuf() expects a jamovi.coms.ResultsElement")
+
+            if (base::any(oChanges %in% private$.clearWith))
+                return()
+
+            for (clearName in private$.clearWith) {
+                if (base::any(vChanges %in% private$.options$option(clearName)$vars))
+                    return()
+            }
+
+            private$.content <- element$preformatted
+        },
+        asProtoBuf=function(incAsText=FALSE, status=NULL) {
+            initProtoBuf()
+            element <- super$asProtoBuf(incAsText=TRUE, status=status)
+            element$preformatted <- private$.content
+            element
+        }
+    )
+)
+
